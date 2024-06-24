30af0d822473a69c53678365af9254442df73666
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-06-24 15:33:12 +0100

    add option for cors

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
   origin:[
-    "http://localhost:5173"
+    "http://localhost:5173",
+    "https://ytlinker.vercel.app"
   ]
 }))
 const transport = nodemailer.createTransport({
